4846890a3318ea409eaa4d74ab3b37a61f9a35f4
Juju6388 <ulia210700@mail.ru>
2021-11-03 23:58:49 +0300

    Шифрование и дешифрование шифрами Цезаря, Виженера и Гронсфельда

---
+++ Шифратор/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Шифратор
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(31, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(147, 212);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(224, 50);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(147, 212);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(410, 50);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(147, 212);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(737, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(737, 156);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(56, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(737, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Исходный текст";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Зашифрованный";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(424, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Расшифрованный";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(616, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 24);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Виженер";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(616, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 24);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Гронсфельд";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(613, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Зашифровать";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(753, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Ключ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(838, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Расшифровать";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(616, 75);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(68, 24);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Цезарь";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(841, 75);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(68, 24);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Цезарь";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(841, 156);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(68, 24);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "Гронсфельд";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(841, 113);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(68, 24);
+            this.button10.TabIndex = 19;
+            this.button10.Text = "Виженер";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(596, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(352, 27);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Выбрать текст из фаайла";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(616, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 24);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Исходный текст";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(832, 202);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(104, 36);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Зашифрованный текст";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // chart1
+            // 
+            chartArea3.AxisX.Crossing = 1.7976931348623157E+308D;
+            chartArea3.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea3.AxisX.LabelAutoFitMinFontSize = 5;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(31, 268);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series3.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1043, 181);
+            this.chart1.TabIndex = 25;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.AxisX.Crossing = 1.7976931348623157E+308D;
+            chartArea4.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea4.AxisX.LabelAutoFitMinFontSize = 5;
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(32, 455);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            series4.YValuesPerPoint = 2;
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(1042, 133);
+            this.chart2.TabIndex = 26;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 618);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Зашифрованный";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Шифратор/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Шифратор
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt files'(*.txt| *.txt| All files (*.*)|*.*)";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                FileStream filestream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+                if (filestream != null)
+                {
+                    StreamReader streamreader = new StreamReader(filestream);
+                    richTextBox1.Text = streamreader.ReadToEnd();
+                    filestream.Close();
+                }
+            }
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            string Alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+            for (int i = 0; i <= 32; i++)
+            {
+                char ai = Convert.ToChar(Alph[i]);
+            }
+            int key = Convert.ToInt32(textBox1.Text);
+            string text = richTextBox1.Text;
+            for (int i = 0; i < text.Length ; i++)
+            {
+                char bi = Convert.ToChar(text[i]);
+                bi = Convert.ToChar(Convert.ToInt32(bi) + key);
+                richTextBox2.Text += Convert.ToString(bi);
+            }
+            
+        }
+        
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Clear();
+            // обрабатываем исходный текст
+            int[] A = new int[33];
+            string text = richTextBox1.Text;
+            string Alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            char[] alp = new char[33];
+            for (int i = 0; i < 33; i++)
+                alp[i] = Convert.ToChar(Alph[i]);
+            for (int i = 0; i < 33; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    char bj = Convert.ToChar(text[j]);
+                    if (bj == alp[i])
+                        A[i]++;
+                }
+            }
+            for (int i = 0; i < 33; i++)
+            {
+                for (int k = i + 1; k < 33; k++)
+                {
+                    if (A[i] < A[k])
+                    {
+                        int temp = A[i];
+                        A[i] = A[k];
+                        A[k] = temp;
+                        char temps = alp[i];
+                        alp[i] = alp[k];
+                        alp[k] = temps;
+                    }
+                }
+            }
+            // обрабатываем зашифрованный тескт 
+            string text2 = richTextBox2.Text;
+            int[] A2 = new int[33];
+            char[] alp2 = new char[33];
+            for (int i = 0; i < 33; i++)
+                alp2[i] = Convert.ToChar(Alph[i]);
+            for (int i = 0; i < 33; i++)
+            {
+                for (int j = 0; j < text2.Length; j++)
+                {
+                    char kj = Convert.ToChar(text2[j]);
+                    if (kj == alp2[i])
+                        A2[i]++;
+                }
+            }
+            for (int i = 0; i < 33; i++)
+            {
+                for (int k = i + 1; k < 33; k++)
+                {
+                    if (A2[i] < A2[k])
+                    {
+                        int temp = A2[i];
+                        A2[i] = A2[k];
+                        A2[k] = temp;
+                        char temps = alp2[i];
+                        alp2[i] = alp2[k];
+                        alp2[k] = temps;
+                    }
+                }
+            }
+             int key =  Convert.ToInt32(alp2[0]) - Convert.ToInt32(alp[0]) ;
+            char[] enc = new char[text2.Length];
+            for (int i=0;i<text2.Length; i++)
+            {
+                enc[i] = Convert.ToChar(Convert.ToInt32(text2[i]) - key);
+                richTextBox3.Text += enc[i];
+            }
+        }
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string Alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            string text2 = richTextBox2.Text;
+            int[] A2 = new int[33];
+            char[] alp2 = new char[33];
+            for (int i = 0; i < 33; i++)
+                alp2[i] = Convert.ToChar(Alph[i]);
+            for (int i = 0; i < 33; i++)
+            {
+                for (int j = 0; j < text2.Length; j++)
+                {
+                    char kj = Convert.ToChar(text2[j]);
+                    if (kj == alp2[i])
+                        A2[i]++;
+                }
+            }
+            for (int i = 0; i < 33; i++)
+            {
+                for (int k = i + 1; k < 33; k++)
+                {
+                    if (A2[i] < A2[k])
+                    {
+                        int temp = A2[i];
+                        A2[i] = A2[k];
+                        A2[k] = temp;
+                        char temps = alp2[i];
+                        alp2[i] = alp2[k];
+                        alp2[k] = temps;
+                    }
+                }
+            }
+            for (int i = 0; i < 33; i++)
+            {
+                chart2.Series[0].Points.AddXY(Convert.ToString(alp2[i]), A2[i]);
+            }
+        }
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[] A = new int[33];
+            string text = richTextBox1.Text;
+            string Alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            char[] alp = new char[33];
+            for (int i = 0; i < 33; i++)
+                alp[i] = Convert.ToChar(Alph[i]);
+            for (int i = 0; i < 33; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    char bj = Convert.ToChar(text[j]);
+                    if (bj == alp[i])
+                        A[i]++;
+                }
+            }
+            for (int i = 0; i < 33; i++)
+            {
+                for (int k = i + 1; k < 33; k++)
+                {
+                    if (A[i] < A[k])
+                    {
+                        int temp = A[i];
+                        A[i] = A[k];
+                        A[k] = temp;
+                        char temps = alp[i];
+                        alp[i] = alp[k];
+                        alp[k] = temps;
+                    }
+                }
+                richTextBox2.Text += alp[i].ToString();
+            }
+            /*  string newal= null;
+              for (int i=0;i<33;i++)
+              {
+                  newal = newal + alp[i].ToString();
+              }
+            */
+            for (int i = 0; i < 33; i++)
+            {
+                chart1.Series[0].Points.AddXY(Convert.ToString(alp[i]), A[i]);
+            }
+        }
+    }
+}
